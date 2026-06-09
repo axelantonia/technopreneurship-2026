9d6a624fdd3bca0dcfab16d34cbc0d3694e7ddde
lindsay <gabriellelindsaysupradja@gmail.com>
2026-06-09 18:00:29 +0700

    fix: login n signup

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
 
 Route::get('/', fn() => view('pages.home'))->name('home');
+Route::get('/home', fn() => redirect('/'));
+Route::get('/tutors/dashboard', fn() => view('tutors.dashboard'))->name('tutors.dashboard');
 
 Route::get('/tutors',          [BookingController::class, 'tutorList'])->name('tutors');
 Route::get('/tutors/{id}',     [BookingController::class, 'tutorDetail'])->name('tutor.detail');
@@ -15,3 +17,5 @@ Route::get('/login',  fn() => view('pages.login'))->name('login');
 Route::get('/signup', fn() => view('pages.signup'))->name('signup');
 Route::get('/chat/{tutor_id?}', [\App\Http\Controllers\ChatController::class, 'index'])->name('chat');
 Route::view('/voucher', 'pages.belivoucher')->name('belivoucher');
+
+
